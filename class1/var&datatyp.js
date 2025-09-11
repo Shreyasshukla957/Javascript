cea58f94a05111faefc83fd53456852745e417ce
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2025-09-12 01:55:18 +0530

    Javascript Class 1 commit

---
+++ class1/var&datatyp.js
@@ -0,0 +1,67 @@
+// var age = 25;
+
+// if(true){
+//   console.log(age);
+// }
+
+
+// // VAR
+// var a = 20;
+// // var can be redefine
+// var a = 30 ;
+// // can be reassigned
+// a = 40;
+ 
+
+// // Let 
+// let b = 20;
+// // let cannot be redefined it'll throw error
+// // let b = 30;
+// // let can be reassigned though
+//  b = 40;
+ 
+
+// //  Const
+// const c = 20;
+// // Const neither can be redefined nor can be declared
+
+
+
+// // Datatypes
+// primitive and non-primitive data types
+
+// Primitive
+// Number , Boolean , String , Null , Undefined , Symbol , BigInt
+
+// Number
+let marks = 20;
+console.log(marks);
+console.log(typeof(marks));
+// String
+marks = "shreyas";
+console.log(marks);
+console.log(typeof(marks));
+// Boolean
+marks=true;
+console.log(marks);
+console.log(typeof(marks));
+// Null
+// null ek 'object' datatype h baaki sab primitive mein own datatype hote h ex:- number jo h 'number' dataype hota h
+marks=null;
+console.log(marks);
+console.log(typeof(marks));
+// Undefined
+let undefined ;
+console.log(undefined);
+console.log(typeof(undefined));
+// BigInt
+let a = 4551213554533434313454651n;
+console.log(a);
+console.log(typeof(a));
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+
+
+
